908c757751d2103abb856e1a303506da5a5376a2
Ankur Gupta <rukna1000@gmail.com>
2016-08-21 15:12:19 -0400

    Changed to multiprocessing... This bad boy goes fast... really fast

--- Measure_Distance.py
+++ Measure_Distance.py
@@ -4,83 +4,73 @@ import os
 import glob
 import matplotlib.pyplot as plot
 import re
-import threading
+import multiprocessing
 from skimage.feature import canny
 from skimage import img_as_ubyte
 
-class new_thread(threading.Thread):
-    def __init__(self, file_paths, tID):
-        threading.Thread.__init__(self)
-        self.file_paths = file_paths
-        self.threadID = tID
 
-    def run(self):
-        figure = plot.figure("Crash Labeller - Thread " + str(self.threadID))
-        image_raw = figure.add_subplot(221)
-        image_edges = figure.add_subplot(222)
-        text = figure.add_subplot(223)
-        text_ax = text.axis([-1, 1, -1, 1])
+def create_figure(path):
+    figure = plot.figure("Crash Labeller")
+    image_raw = figure.add_subplot(221)
+    image_edges = figure.add_subplot(222)
+    text = figure.add_subplot(223)
+    text_ax = text.axis([-1, 1, -1, 1])
 
-        for path in files:
-            int_values = []
-            path_sections = path.split("/")
+    int_values = []
+    path_sections = path.split("/")
 
-            for string in path_sections:
-                if re.search(r'\d+', string) is not None:
-                    int_values.append(int(re.search(r'\d+', string).group()))
+    for string in path_sections:
+        if re.search(r'\d+', string) is not None:
+            int_values.append(int(re.search(r'\d+', string).group()))
 
-            file_count = int_values[-1]
-            crash = False
+    file_count = int_values[-1]
+    crash = False
 
-            print file_count
+    print file_count
 
-            image = cv2.imread(path)
+    image = cv2.imread(path)
 
-            img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            edges = img_as_ubyte(canny(img_gray, sigma=canny_sigma))
-            img_bw = cv2.threshold(edges, 250, 255, cv2.THRESH_BINARY)[1]
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = img_as_ubyte(canny(img_gray, sigma=canny_sigma))
+    img_bw = cv2.threshold(edges, 250, 255, cv2.THRESH_BINARY)[1]
 
-            point = self.find_bottom_edge(img_bw)
-            distance = np.sqrt(np.power(point[0] - len(img_bw[0]) / 2, 2) + np.power(point[1] - len(img_bw), 2))
+    point = find_bottom_edge(img_bw)
+    distance = np.sqrt(np.power(point[0] - len(img_bw[0]) / 2, 2) + np.power(point[1] - len(img_bw), 2))
 
-            cv2.line(image, point, (len(img_bw[0]) / 2, len(img_bw)), (0, 255, 0), 1)
+    cv2.line(image, point, (len(img_bw[0]) / 2, len(img_bw)), (0, 255, 0), 1)
 
-            if distance <= _threshold:
-                crash = True
+    if distance <= _threshold:
+        crash = True
 
-            image_raw.imshow(image)
-            image_raw.set_title("Image raw")
-            image_edges.imshow(edges)
-            image_edges.set_title("Edges")
-            crash_status = text.text(-0.5, 0.5, "Crash Status: " + str(crash))
-            distance_status = text.text(-0.5, -0.5, "Distance: " + str(distance))
+    image_raw.imshow(image)
+    image_raw.set_title("Image raw")
+    image_edges.imshow(edges)
+    image_edges.set_title("Edges")
+    crash_status = text.text(-0.5, 0.5, "Crash Status: " + str(crash))
+    distance_status = text.text(-0.5, -0.5, "Distance: " + str(distance))
 
-            figure.savefig("figures_sigma_" + str(canny_sigma) + "/FRAME_" + str(file_count))
-            crash_status.remove()
-            distance_status.remove()
+    figure.savefig("figures_sigma_" + str(canny_sigma) + "/FRAME_" + str(file_count))
+    crash_status.remove()
+    distance_status.remove()
 
-    def find_bottom_edge(self, img_bw):
-        for i in reversed(xrange(len(img_bw))):
-            for j in xrange(len(img_bw[0])):
-                if(img_bw[i][j] > 0):
-                    point = (j, i)
-                    return point
+def find_bottom_edge(img_bw):
+    for i in reversed(xrange(len(img_bw))):
+        for j in xrange(len(img_bw[0])):
+            if(img_bw[i][j] > 0):
+                point = (j, i)
+                return point
 
 _threshold = 30
 canny_sigma = 2.5
-threads = 3
+threads = 6
 
 plot.ion()
 os.mkdir(os.getcwd() + "/figures_sigma_" + str(canny_sigma))
+files = glob.glob("/home/agupta/Documents/Projects/Feature-Tracker/bw150/*")
 
-files = glob.glob(os.getcwd() + "/resize150/*")
-threads_list = []
+print files
 
-for i in xrange(threads):
-    threads_list.append(new_thread(files[len(files) / threads * i:len(files) / threads * (i + 1) - 1], i))
+pool = multiprocessing.Pool(threads)
+pool.map(create_figure, files)
 
-for delegate in threads_list:
-    delegate.start()
-
-for delegate in threads_list:
-    delegate.join()
+#create_figure(files)
