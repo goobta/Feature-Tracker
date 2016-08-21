3a4a7c83feb3182c961c5d7b7274f8fcfd633b16
Ankur Gupta <rukna1000@gmail.com>
2016-08-21 12:49:12 -0400

    Made search algorithm into function for easier loop breaking

--- Measure_Distance.py
+++ Measure_Distance.py
@@ -5,11 +5,28 @@ import glob
 from skimage.feature import canny
 from skimage import img_as_ubyte
 
+def find_bottom_edge(img_bw):
+    for i in reversed(xrange(len(img_bw))):
+        for j in xrange(len(img_bw[0])):
+            print i, j, img_bw[i][j]
+
+            if(img_bw[i][j] > 0):
+                point = (j, i)
+                print point
+
+                return point
+
 files = glob.glob(os.getcwd() + "/bw150/*")
 
 for path in files:
-    image = cv2.imread(path)
-    edges = img_as_ubyte(canny(image, sigma=2))
+    image = cv2.imread(path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    edges = img_as_ubyte(canny(image, sigma=3))
+    img_bw = cv2.threshold(edges, 250, 255, cv2.THRESH_BINARY)[1]
 
-    cv2.imshow("Edges", edges)
+    point = find_bottom_edge(img_bw)
+
+    cv2.line(img_bw, point, (len(img_bw[0]) / 2, len(img_bw)), 255, 1)
+
+    cv2.imshow("Image", image)
+    cv2.imshow("Edges", img_bw)
     cv2.waitKey(0)
