5036f7b48f3b881f3eb3ce51d8870712c60afc1f
Ankur Gupta <rukna1000@gmail.com>
2016-08-21 16:01:16 -0400

    Put the whole thing in a try catch

--- Measure_Distance.py
+++ Measure_Distance.py
@@ -10,48 +10,52 @@ from skimage import img_as_ubyte
 
 
 def create_figure(path):
-    figure = plot.figure("Crash Labeller")
-    image_raw = figure.add_subplot(221)
-    image_edges = figure.add_subplot(222)
-    text = figure.add_subplot(223)
-    text_ax = text.axis([-1, 1, -1, 1])
+    try:
+        figure = plot.figure("Crash Labeller")
+        image_raw = figure.add_subplot(221)
+        image_edges = figure.add_subplot(222)
+        text = figure.add_subplot(223)
+        text_ax = text.axis([-1, 1, -1, 1])
 
-    int_values = []
-    path_sections = path.split("/")
+        int_values = []
+        path_sections = path.split("/")
 
-    for string in path_sections:
-        if re.search(r'\d+', string) is not None:
-            int_values.append(int(re.search(r'\d+', string).group()))
+        for string in path_sections:
+            if re.search(r'\d+', string) is not None:
+                int_values.append(int(re.search(r'\d+', string).group()))
 
-    file_count = int_values[-1]
-    crash = False
+        file_count = int_values[-1]
+        crash = False
 
-    print file_count
+        print file_count
 
-    image = cv2.imread(path)
+        image = cv2.imread(path)
 
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = img_as_ubyte(canny(img_gray, sigma=canny_sigma))
-    img_bw = cv2.threshold(edges, 250, 255, cv2.THRESH_BINARY)[1]
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        edges = img_as_ubyte(canny(img_gray, sigma=canny_sigma))
+        img_bw = cv2.threshold(edges, 250, 255, cv2.THRESH_BINARY)[1]
 
-    point = find_bottom_edge(img_bw)
-    distance = np.sqrt(np.power(point[0] - len(img_bw[0]) / 2, 2) + np.power(point[1] - len(img_bw), 2))
+        point = find_bottom_edge(img_bw)
+        distance = np.sqrt(np.power(point[0] - len(img_bw[0]) / 2, 2) + np.power(point[1] - len(img_bw), 2))
 
-    cv2.line(image, point, (len(img_bw[0]) / 2, len(img_bw)), (0, 255, 0), 1)
+        cv2.line(image, point, (len(img_bw[0]) / 2, len(img_bw)), (0, 255, 0), 1)
 
-    if distance <= _threshold:
-        crash = True
+        if distance <= _threshold:
+            crash = True
 
-    image_raw.imshow(image)
-    image_raw.set_title("Image raw")
-    image_edges.imshow(edges)
-    image_edges.set_title("Edges")
-    crash_status = text.text(-0.5, 0.5, "Crash Status: " + str(crash))
-    distance_status = text.text(-0.5, -0.5, "Distance: " + str(distance))
+        image_raw.imshow(image)
+        image_raw.set_title("Image raw")
+        image_edges.imshow(edges)
+        image_edges.set_title("Edges")
+        crash_status = text.text(-0.5, 0.5, "Crash Status: " + str(crash))
+        distance_status = text.text(-0.5, -0.5, "Distance: " + str(distance))
 
-    figure.savefig("figures_sigma_" + str(canny_sigma) + "/FRAME_" + str(file_count))
-    crash_status.remove()
-    distance_status.remove()
+        figure.savefig("figures_sigma_" + str(canny_sigma) + "/FRAME_" + str(file_count))
+        crash_status.remove()
+        distance_status.remove()
+
+    except Exception:
+        print "Error with file"
 
 def find_bottom_edge(img_bw):
     for i in reversed(xrange(len(img_bw))):
@@ -62,7 +66,7 @@ def find_bottom_edge(img_bw):
 
 _threshold = 30
 canny_sigma = 2.5
-threads = 6
+threads = 8
 
 plot.ion()
 os.mkdir(os.getcwd() + "/figures_sigma_" + str(canny_sigma))
